7f9d842883bc125a2f9d84232e00a555cd2b6168
rmartin04 <ricarditomartin1979+1@gmail.com>
2025-05-13 20:20:29 +0200

    primer ejercicio con servlet

--- ProyectoWeb/src/main/java/es/cursojava/web/AlumnoServlet.java
+++ ProyectoWeb/src/main/java/es/cursojava/web/AlumnoServlet.java
@@ -3,6 +3,7 @@ package es.cursojava.web;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,6 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AlumnoServlet
  */
+@WebServlet("/alumno")
 public class AlumnoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -24,23 +26,34 @@ public class AlumnoServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Entrando en doGet de AlumnoServlet");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	
+		//doGet(request, response);
+		System.out.println("Entrando en doPost de AlumnoServlet");
 	}
 
 	@Override
 	public void init() throws ServletException {
-		// TODO Auto-generated method stub
-		super.init();
+	
+		//super.init();
+		System.out.println("Entrando en init de AlumnoServlet");
 	}
+
+	@Override
+	public void destroy() {
+		
+		//super.destroy();
+		System.out.println("Destruyendo AlumnoServlet");
+	}
+	
 	
 
 }
---
+++ ProyectoWeb/src/main/java/es/cursojava/web/CalculadoraServlet.java
@@ -0,0 +1,76 @@
+package es.cursojava.web;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/calculadora")
+public class CalculadoraServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Default constructor.
+	 */
+	public CalculadoraServlet() {
+		
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Entrando en doGet de CalculadoraServlet");
+
+		String num1 = req.getParameter("num1");
+		String num2 = req.getParameter("num2");
+		String operacion = req.getParameter("operacion");
+
+		int resultado = 0;
+
+		switch (operacion) {
+		case "suma":
+			resultado = Integer.parseInt(num1) + Integer.parseInt(num2);
+			break;
+		case "resta":
+			resultado = Integer.parseInt(num1) - Integer.parseInt(num2);
+			break;
+		case "multiplicacion":
+			resultado = Integer.parseInt(num1) * Integer.parseInt(num2);
+			break;
+		case "division":
+			resultado = Integer.parseInt(num1) / Integer.parseInt(num2);
+			break;
+		default:
+			break;
+		}
+
+		resp.getWriter().append("Resultado: ").append(String.valueOf(resultado));
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		super.doPost(req, resp);
+	}
+
+	@Override
+	public void init() throws ServletException {
+		
+		super.init();
+	}
+
+	@Override
+	public void destroy() {
+	
+		super.destroy();
+	}
+	
+
+	
+
+}
