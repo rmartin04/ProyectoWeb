7d5852cc9a816d4ecce29f901db78acc40e615f1
rmartin04 <ricarditomartin1979+1@gmail.com>
2025-05-16 19:37:31 +0200

    hibernate en el proyecto wweb

---
+++ ProyectoWeb/src/main/java/es/cursojava/web/utils/HibernateUtil.java
@@ -0,0 +1,44 @@
+package es.cursojava.web.utils;
+
+import javax.security.auth.login.Configuration;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class HibernateUtil {
+    //private static final Session session;
+    private static Session session;
+
+    // static{
+    //     try {
+    //         SessionFactory sessionFactory = new Configuration()
+    //                 .configure() // Carga hibernate.cfg.xml
+    //                 .buildSessionFactory();
+    //         session = sessionFactory.openSession();
+    //     } catch (Throwable ex) {
+    //         System.err.println("Error al crear la SessionFactory." + ex);
+    //         throw new ExceptionInInitializerError(ex);
+    //     }
+    // }
+
+    private static Session generateSession (){ 
+        try {
+            SessionFactory sessionFactory = new Configuration()
+                    .configure() // Carga hibernate.cfg.xml
+                    .buildSessionFactory();
+            session = sessionFactory.openSession();
+        } catch (Throwable ex) {
+            System.err.println("Error al crear la SessionFactory." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+        return session;
+    }
+
+    public static Session getSession() {
+        if (session == null) {
+            session = generateSession();
+        }
+       
+        return session;
+    }
+}
